4a01c21db252fd896959247b0ebb805b9fe11e6c
Carlos Augusto <carlosaaugusto2002@outlook.com>
2024-05-15 21:59:33 -0300

    UPDT: Todo Ook

--- src/components/taskTable/taskTable.tsx
+++ src/components/taskTable/taskTable.tsx
@@ -5,8 +5,10 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 import { Button } from "../ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Trash2 } from 'lucide-react';
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import { formatDate } from "@/service/utils";
 
 interface Props {
   tasks: Task[];
@@ -21,7 +23,10 @@ export default function TaskTable({ tasks, complete, exclude }: Props) {
     <main className="w-full flex-wrap flex flex-row justify-center lg:justify-normal ">
       {tasks.map((task, index) => {
         return (
-          <Card key={index} className="w-[80%] lg:ml-[4%] my-[2%] lg:w-[20%] ">
+          <Card key={index} className="relative w-[80%] lg:ml-[4%] my-[2%] lg:w-[20%] ">
+            {!task.completed &&
+              <Trash2 onClick={() => { exclude(task.id) }} className="absolute left-[90%] top-[5%] hover:text-red-500 transition-transform"/>
+            }
 
             <CardHeader>
               <CardTitle>{task.title}</CardTitle>
@@ -29,24 +34,28 @@ export default function TaskTable({ tasks, complete, exclude }: Props) {
             </CardHeader>
 
             <CardContent>
-              <p>Created At : {task.created_at.toString()}</p>
+              <p>Created At : {formatDate(task.created_at)}</p>
               <p>Status : {task.completed ? 'Complete' : 'Pending'}</p>
             </CardContent>
 
             <CardFooter>
+              {!task.completed &&
+                (
+                  <Popover>
+                    <PopoverTrigger className="w-full" asChild>
+                      <Button variant="outline">Concluir</Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[100%]">
+                      <h1>Concluir essa tarefa?</h1>
+                      <div className="w-full flex flex-row justify-evenly items-center">
+                        <Button className="w-[45%]" onClick={() => { complete(task.id) }}>Sim</Button>
+                        <Button className="w-[45%]">Não</Button>
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                )
+              }
 
-              <Popover>
-                <PopoverTrigger className="w-full"  asChild>
-                  <Button variant="outline">Concluir</Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-[100%]">
-                  <h1>Concluir essa tarefa?</h1>
-                  <div className="w-full flex flex-row justify-evenly items-center">
-                    <Button className="w-[45%]" onClick={() => { complete(task.id)}}>Sim</Button>
-                    <Button className="w-[45%]">Não</Button>
-                  </div>
-                </PopoverContent>
-              </Popover>
 
 
 
